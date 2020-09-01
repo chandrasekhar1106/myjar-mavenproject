eb334bf220c91ee5fb6070b97fca04d043f4f478
chandrasekhar <chandraganimisetty@gmail.com>
2020-09-01 11:14:19 +0530

    Adding all files to local repo

---
+++ src/main/java/chandrasekhar/App.java
@@ -0,0 +1,13 @@
+package chandrasekhar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Chandrasekhar!! Welcome to java...Its version-1.." );
+    }
+}
