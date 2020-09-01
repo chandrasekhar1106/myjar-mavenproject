351caff9d8c7dfa29ce5ecbb3984809466b126e0
chandrasekhar <chandraganimisetty@gmail.com>
2020-09-01 12:08:23 +0530

    re-updated version-7

--- src/main/java/chandrasekhar/App.java
+++ src/main/java/chandrasekhar/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Chandrasekhar!! Welcome to java...Its version-6.." );
+        System.out.println( "Hello Chandrasekhar!! Welcome to java...Its version-7.." );
     }
 }
