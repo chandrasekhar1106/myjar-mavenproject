b965d9e76c0f7817ae3082849220c06813dbd33b
chandrasekhar1106 <64906323+chandrasekhar1106@users.noreply.github.com>
2020-09-02 11:14:09 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,16 @@
 def buildApp() {
 	echo 'Building the Application'
 }
+
+def testApp() {
+	echo 'Testing the Application..'
+}
+
+def installApp() {
+	echo 'Installing the Application..'
+}
+
+def deployApp() {
+	echo 'Deploying the application..'
+}
 return this
